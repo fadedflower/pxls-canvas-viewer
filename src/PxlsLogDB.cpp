7fb4cdab2ee85ebcaafc6e909a86fb1b5d2ffd2b
WinTP <1789016472@qq.com>
2024-12-25 16:36:48 +0800

    create a basic raylib application

---
+++ src/PxlsLogDB.cpp
@@ -0,0 +1,5 @@
+//
+// Created by WinTP on 24-12-25.
+//
+
+#include "PxlsLogDB.h"
---
+++ src/PxlsLogDB.h
@@ -0,0 +1,16 @@
+//
+// 提供了将
+//
+
+#ifndef PXLSLOGDB_H
+#define PXLSLOGDB_H
+
+
+
+class PxlsLogDB {
+
+};
+
+
+
+#endif //PXLSLOGDB_H
